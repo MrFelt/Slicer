598f32a2259d36a5a5977beeccf29c851ad0ec78
MrFelt <116107934+MrFelt@users.noreply.github.com>
2023-08-24 13:25:08 -0700

    asd

--- init.py
+++ init.py
@@ -1,10 +1,22 @@
 import os
 import subprocess
 import sys
-import whisper
+import logging
+
+# Set up logging
+log_directory = 'logs'
+if not os.path.exists(log_directory):
+    os.makedirs(log_directory)
+
+logging.basicConfig(
+    filename=os.path.join(log_directory, 'init.log'),
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
 
 def find_audio_files(directory):
-    audio_extensions = ['.mp3', '.wav', '.flac']  # Add more extensions if needed
+    logging.info(f'Searching for audio files in {directory}')
+    audio_extensions = ['.mp3', '.wav', '.flac']
     audio_files = []
 
     for root, _, files in os.walk(directory):
@@ -13,10 +25,12 @@ def find_audio_files(directory):
             if extension.lower() in audio_extensions:
                 audio_files.append(os.path.join(root, file))
 
+    logging.info(f'Found {len(audio_files)} audio files')
     return audio_files
 
-
 def main():
+    logging.info('Initiating the process')
+
     if len(sys.argv) != 2:
         print("Usage: python main_script.py <directory_path>")
         return
@@ -33,18 +47,17 @@ def main():
         print("No audio files found in the given directory.")
         return
 
-        resample_script = "resample.py"
+    resample_script = "resample.py"
 
-        for audio_file in audio_files:
-            subprocess.run(["python", resample_script, audio_file])
+    for audio_file in audio_files:
+        logging.info(f'Running resample.py on {audio_file}')
+        subprocess.run(["python", resample_script, audio_file])
 
-            # Assuming the resample script generates a resampled audio file in the same directory
-            resampled_audio_file = os.path.join(os.path.dirname(audio_file),
-                                                "resampled_" + os.path.basename(audio_file))
+        resampled_audio_file = os.path.join(os.path.dirname(audio_file),
+                                            "resampled_" + os.path.basename(audio_file))
+        logging.info(f'Running whisper.py on {resampled_audio_file}')
+        whisper_script = "whisper.py"
+        subprocess.run(["python", whisper_script, resampled_audio_file])
 
-            # Call whisper.py here
-            whisper_script = "whisper.py"
-            subprocess.run(["python", whisper_script, resampled_audio_file])
-
-    if __name__ == "__main__":
-        main()
+if __name__ == "__main__":
+    main()
--- resample.py
+++ resample.py
@@ -2,9 +2,21 @@ import os
 import subprocess
 import sys
 import concurrent.futures
+import logging
 
+# Set up logging
+log_directory = 'logs'
+if not os.path.exists(log_directory):
+    os.makedirs(log_directory)
+
+logging.basicConfig(
+    filename=os.path.join(log_directory, 'resample.log'),
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
 
 def resample_audio(input_audio):
+    logging.info(f'Starting resampling for {input_audio}')
     try:
         # Using FFmpeg to resample and convert to FLAC format
         command = [
@@ -12,12 +24,14 @@ def resample_audio(input_audio):
             "-sample_fmt", "s32", "-b:a", "256k", "-f", "null", "-"
         ]
         subprocess.run(command, check=True)
-        print(f"Resampled: {input_audio}")
+        logging.info(f"Resampled: {input_audio}")
     except subprocess.CalledProcessError as e:
+        logging.error(f"Error resampling {input_audio}: {e}")
         print(f"Error resampling {input_audio}: {e}")
 
-
 def main():
+    logging.info('Initiating resampling process')
+
     if len(sys.argv) < 2:
         print("Usage: python resample.py <audio_file1> [<audio_file2> ...]")
         return
@@ -26,13 +40,15 @@ def main():
 
     cpu_count = os.cpu_count()
     max_workers = int((cpu_count / 2) * 0.4)  # Calculate max_workers based on CPU thread count
-
     max_workers = max(max_workers, 1)  # Ensure at least 1 worker
+
+    logging.info(f'Starting resampling with {max_workers} workers')
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
         for input_audio in input_audio_files:
             executor.submit(resample_audio, input_audio)
 
+    logging.info('Resampling process completed')
 
 if __name__ == "__main__":
     main()
