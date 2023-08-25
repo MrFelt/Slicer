9deb64b45dff5bb28a7c0d97dd0c108a299c223f
MrFelt <116107934+MrFelt@users.noreply.github.com>
2023-08-25 03:42:13 -0700

    update inst / run bats

--- install.bat
+++ install.bat
@@ -1,4 +1,13 @@
-call .\venv\Scripts\activate
-call python -m pip install -r requirements.txt
+@echo off
 
-pause
+REM Check if the virtual environment exists
+if not exist ".\venv" (
+    echo Creating virtual environment...
+    call python -m venv venv
+    call .\venv\Scripts\activate
+    call python -m pip install -r requirements.txt
+    pause
+) else (
+    echo Virtual environment already exists, running start.bat...
+    call start.bat
+)
--- start.bat
+++ start.bat
@@ -1,3 +1,9 @@
+@echo off
+
+echo Activating virtual environment...
 call .\venv\Scripts\activate
+
+echo Running init.py...
 call python init.py
+
 pause
