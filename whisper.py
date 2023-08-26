ee2f3082a246f2e5a0cc5c286f6d458bb2df38ad
MrFelt <116107934+MrFelt@users.noreply.github.com>
2023-08-26 09:14:24 -0700

    Update whisper.py

--- whisper.py
+++ whisper.py
@@ -36,7 +36,7 @@ else:
 
 def process_audio(audio_path, output_folder):
     model_name = "large-v2"
-    mtypes = {'cpu': 'int8', 'cuda': 'float16'}
+    mtypes = {'cpu': 'int8', 'cuda': 'int8'}
 
     logging.info(f"Processing audio file {audio_path}")
     logging.info(f"Using device: {device}")
@@ -51,12 +51,13 @@ def process_audio(audio_path, output_folder):
         audio_path,
         vad_filter=True,
         vad_parameters=dict(
-            threshold=0.65,
+            threshold=0.95,
             min_silence_duration_ms=80,
             min_speech_duration_ms=1000,
             speech_pad_ms=0,
             window_size_samples=1024
-        )
+        ),
+        beam_size=5
     )
 
     # Read the audio file
